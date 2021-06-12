3bcafab30914dd02108f53839cf2ff6daa4f3336
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-06-12 19:25:13 +0200

    update task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -14,6 +14,7 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
 
 import { TasksService } from './tasks.service';
@@ -47,11 +48,11 @@ export class TasksController {
     return this.tasksServise.deleteTask(id);
   }
 
-  // @Patch('/:id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  // ): Task {
-  //   return this.tasksServise.updateTaskStatus(id, status);
-  // }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.tasksServise.updateTaskStatus(id, status);
+  }
 }
