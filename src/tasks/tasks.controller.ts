1579fd029351940cd65f31d3c283ff122e728e4c
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-06-12 19:56:15 +0200

    get all tasks without filteration

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -23,14 +23,12 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private tasksServise: TasksService) {}
 
-  // @Get()
-  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-  //   if (Object.keys(filterDto).length) {
-  //     return this.tasksServise.getTasksWithFilters(filterDto);
-  //   } else {
-  //     return this.tasksServise.getAllTasks();
-  //   }
-  // }
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+  ): Promise<Task[]> {
+    return this.tasksServise.getTasks(filterDto);
+  }
 
   @Get('/:id')
   getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
