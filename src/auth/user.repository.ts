399e7651b71f4c76c1d64d56537f1faec6e4b4e9
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-06-13 02:09:44 +0200

    SignIn

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -11,11 +11,11 @@ import { User } from './user.entity';
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const salt = await bcrypt.genSalt();
 
     const user = new User();
     user.username = username;
-    user.password = await this.hashPassword(password, salt);
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
     try {
       await user.save();
     } catch (err) {
@@ -24,6 +24,20 @@ export class UserRepository extends Repository<User> {
       else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async validationUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
     }
   }
   private async hashPassword(password: string, salt: string): Promise<string> {
