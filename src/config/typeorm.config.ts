7cf0ff0f813bebdba990198d4c64f52039e444c6
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-06-13 22:18:39 +0200

    add configuration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,4 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+const dbConfig = config.get('db');
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as config from 'config';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const serverConfig = config.get('server');
+  const port = process.env.port || serverConfig.port;
   await app.listen(port);
   logger.log(`Application Listening on port ${port}`);
 }
